01c9e447b8130b4d6438b85249dabd26cdb432cd
armandogb <armandogbulnesjr@gmail.com>
2015-12-20 20:52:36 -0600

    home template

---
+++ templates/home-template.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template Name: Home
+ */
+?>
+
+<section class="home-banner">
+	<div class="home-banner-text">
+	</div>
+	<div class="home-services-but">
+	</div>
+<seciton>
+<section class="home-giant-menu">
+</section>
