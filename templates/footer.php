631f80b9e0ff951f0c629aa1f46afd2ecea78167
Armando Bulnes <armandogbulnesjr@gmail.com>
2015-12-22 11:31:55 -0600

    added some more styling on the home page

--- templates/footer.php
+++ templates/footer.php
@@ -5,14 +5,22 @@
 				<img src="<?php echo get_template_directory_uri() . '/dist/images/logo.png'; ?>">
 			</div>
 			<div class="address-bar-foot">
+				<p>Address Address Address Address Address Address Address</p>
 			</div>
 			<div class="rights-reserved-foot">
+				<p>Rights Rights Rights Rights Rights Rights</p>
 			</div>
 		</div>
 		<div class="footer-right">
-			<div class="search-bar">
+			<div class="email-title">
+				<p>Sign up for our mailing list</p>
+				<form class="email-form">
+					<input class="email-field" type="text" name="email" placeholder="Email">
+					<input class="email-submit "type="submit" value="Submit">
+				</form>
 			</div>
 			<div class="social-media">
+				<a href="/contact-us"><img src="<?php echo get_template_directory_uri() . '/dist/images/social_media_proxy.png'; ?>"></a>
 			</div>
 		</div>
 	</section>
--- templates/header.php
+++ templates/header.php
@@ -1,8 +1,12 @@
-<header class="banner">
+<header>
+	<div class="header">
       <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation','menu_class' => 'nav-menu']);
-      endif;
-      ?>
-
+	      if (has_nav_menu('primary_navigation')) :
+	        wp_nav_menu(['theme_location' => 'primary_navigation','menu_class' => 'nav-menu']);
+	      endif;
+	      ?>
+      	<div class="header-logo">
+      		<a href="/"><img src="<?php echo get_template_directory_uri() . '/dist/images/logo.png'; ?>"></a>
+      	</div>
+     </div>
 </header>
