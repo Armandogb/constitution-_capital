3380c3261bd5f4c0d2e42568c04b11105c777031
armandogb <armandogbulnesjr@gmail.com>
2015-12-21 22:26:21 -0600

    added some assets and some more styling

--- templates/footer.php
+++ templates/footer.php
@@ -1,10 +1,19 @@
 
   	<section class="main-footer">
-		<div class="logo-bar-foot">
+  		<div class="left-footer">
+			<div class="logo-bar-foot">
+				<img src="<?php echo get_template_directory_uri() . '/dist/images/logo.png'; ?>">
+			</div>
+			<div class="address-bar-foot">
+			</div>
+			<div class="rights-reserved-foot">
+			</div>
 		</div>
-		<div class="address-bar-foot">
-		</div>
-		<div class="rights-reserved-foot">
+		<div class="footer-right">
+			<div class="search-bar">
+			</div>
+			<div class="social-media">
+			</div>
 		</div>
 	</section>
 
--- templates/template-home.php
+++ templates/template-home.php
@@ -9,15 +9,28 @@
 		<p> example example example example example example example example example example example example.</p>
 	</div>
 	<div class="home-services-but">
-		<p>Services</p>
+		<p>SERVICES</p>
 	</div>
+	<img src="<?php echo get_template_directory_uri() . '/dist/images/stock_background.png'; ?>">
+
 </section>
 
 <section class="home-giant-menu">
 	<div class="home-big-about big-menu">
+		<div class="big-icon">
+			<a href="/about"><img src="<?php echo get_template_directory_uri() . '/dist/images/big_about.png'; ?>"></a>
+			<div class="big-icon-hover">
+			</div>
+		</div>
 	</div>
 	<div class="home-big-services big-menu">
+		<div class="big-icon">
+			<a href="/services"><img src="<?php echo get_template_directory_uri() . '/dist/images/big_services.png'; ?>"></a>
+		</div>
 	</div>
 	<div class="home-big-contact big-menu">
+		<div class="big-icon">
+			<a href="/contact-us"><img src="<?php echo get_template_directory_uri() . '/dist/images/big_contact.png'; ?>"></a>
+		</div>
 	</div>
 </section>
