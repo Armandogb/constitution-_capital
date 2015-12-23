e34e7895feac1379f7f7095d6d79e95ee6952ba5
armandogb <armandogbulnesjr@gmail.com>
2015-12-23 13:27:34 -0600

    added construction template

---
+++ templates/template-construction.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: UnderConstruction
+ */
+?>
+
+<section class="big-banner">
+	<div class="big-banner-text">
+		<p> Under Construction</p>
+	</div>
+	<div class="big-banner-border-line">
+	</div>
+</section>
+<section class="big-banner">
+	<div class="big-banner-text">
+		<p> </p>
+	</div>
+</section>
+<section class="big-banner">
+	<div class="big-banner-text">
+		<p> </p>
+	</div>
+</section>
