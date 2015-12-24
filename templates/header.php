0356ded2337abfb6d1bc332f3317638de07896a5
armandogb <armandogbulnesjr@gmail.com>
2015-12-24 17:47:24 -0600

    added some styling updates

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,14 @@
-<header>
+
 	<div class="header">
       <?php
 	      if (has_nav_menu('primary_navigation')) :
 	        wp_nav_menu(['theme_location' => 'primary_navigation','menu_class' => 'nav-menu']);
 	      endif;
 	      ?>
-      	<div class="header-logo">
+     	<div class="header-logo">
       		<a href="/"><img src="<?php echo get_template_directory_uri() . '/dist/images/logo.png'; ?>"></a>
-      	</div>
+      	</div> 
      </div>
-</header>
+     <section class="header-spacer">
+     </section>
+
