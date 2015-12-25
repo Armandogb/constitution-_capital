67596a78cf7d2d55b59c9bded503376ac9bd35af
armandogb <armandogbulnesjr@gmail.com>
2015-12-25 17:04:58 -0600

    added some contact us page styling

--- templates/contact-us-page.php
+++ templates/contact-us-page.php
@@ -5,17 +5,17 @@
 ?>
 
 <section class="contact-us-location">
-	<div class="contact-us-headline">
+	<div class="contact-us-info">
+		<div class="contact-us-headline">
+		</div>
+		<div class="contact-us-address">
+		</div>
+		<div class="contact-us-phone">
+		</div>
+		<div class="contact-us-email">
+		</div>
 	</div>
-	<div class="contact-us-address">
-	</div>
-	<div class="contact-us-phone">
-	</div>
-	<div class="contact-us-email">
-	</div>
-	<div class="contact-us-map">
-	</div>
-<seciton>
+</section>
 <section class="contact-us-form">
 	<p class="form-text-contactus"></p>
 </section>
