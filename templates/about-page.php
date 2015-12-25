55d37e4053dc50f415cd8110a8ccf1e359d76b99
armandogb <armandogbulnesjr@gmail.com>
2015-12-25 01:39:08 -0600

    added some about page styling

--- templates/about-page.php
+++ templates/about-page.php
@@ -20,12 +20,48 @@
 	</div>
 </section>
 <section class="about-leadership">
-	<div class="leadership-headline">
+	<div class="leadership-left">
+		<div class="leadership-headline">
+			<p>Title Title Title Title Title Title Title Title Title </p>
+		</div>
+		<div class="leadership-paragraph">
+			<p>Words Words Words Words Words Words Words Words Words Words Words Words 
+			Words Words Words Words Words Words Words Words Words Words Words 
+			Words Words Words Words Words Words Words Words Words Words Words </p>
+		</div>
 	</div>
-	<div class="leadership-paragraph">
-	</div>
-	<div class="leadership-personnel">
-		<div class="leader">
+	<div class="leadership-right">
+		<div class="leadership-personnel">
+			<div class="leader-profile">
+				<div class="leader-image">
+				</div>
+				<div class="leader-info">
+					<p class="leader-name">NAME NAME</p>
+					<p class="leader-title">TITLE</p>
+					<p class="leader-blurb">Words Words Words Words Words Words Words Words Words Words Words Words Words Words 
+						Words Words Words Words Words Words Words Words </p>
+				</div>
+			</div>
+			<div class="leader-profile">
+				<div class="leader-image">
+				</div>
+				<div class="leader-info">
+					<p class="leader-name">NAME NAME</p>
+					<p class="leader-title">TITLE</p>
+					<p class="leader-blurb">Words Words Words Words Words Words Words Words Words Words Words Words Words Words 
+						Words Words Words Words Words Words Words Words </p>
+				</div>
+			</div>
+			<div class="leader-profile">
+				<div class="leader-image">
+				</div>
+				<div class="leader-info">
+					<p class="leader-name">NAME NAME</p>
+					<p class="leader-title">TITLE</p>
+					<p class="leader-blurb">Words Words Words Words Words Words Words Words Words Words Words Words Words Words 
+						Words Words Words Words Words Words Words Words </p>
+				</div>
+			</div>
 		</div>
 	</div>
 </section>
