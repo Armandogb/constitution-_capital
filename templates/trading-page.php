4d7dc23301944dd0a9befaa0830eee59d279503a
armandogb <armandogbulnesjr@gmail.com>
2015-12-28 04:43:44 -0600

    styling updates

--- templates/trading-page.php
+++ templates/trading-page.php
@@ -5,8 +5,59 @@
 ?>
 
 <section class="trading-header">
-<seciton>
+	<div class="trading-title left">
+		<p>Trading: days trading</p>
+	</div>
+	<div class="trading-menu">
+	</div>
+</section>
 <section class="trading-column-names">
+	<div class="house-col">
+		<p>Word</p>
+	</div>
+	<div class="middle-columns left">
+		<div class="buy-sell-col">
+			<p>Word</p>
+		</div>
+		<div class="quantity-col">
+			<p>Word</p>
+		</div>
+		<div class="codes-col">
+			<p>Word</p>
+		</div>
+		<div class="strike-col">
+			<p>Word</p>
+		</div>
+		<div class="cp-col">
+			<p>Word</p>
+		</div>
+		<div class="premuim-col">
+			<p>Word</p>
+		</div>
+		<div class="spread-col">
+			<p>Word</p>
+		</div>
+		<div class="comments-col">
+			<p>Word</p>
+		</div>
+	</div>
+	<div class="timestamp-col">
+		<p>Word</p>
+	</div>
 </section>
 <section class="trading-table">
+	<div class="stock-object">
+		<div class="table-blank-space">
+		</div>
+		<div class="house-space">
+		</div>
+		<div class="buy-sell-space">
+		</div>
+		<div class="quantity-space">
+		</div>
+		<div class="timestamp-space">
+		</div>
+		<div class="table-blank-space right">
+		</div>
+	</div>
 </section>
