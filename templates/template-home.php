f280ebe2fbcad0c5834457efc6a05b2cd1821a26
Armando Bulnes <armandogbulnesjr@gmail.com>
2016-01-04 16:40:11 -0600

    added a new home email template

--- templates/template-home.php
+++ templates/template-home.php
@@ -4,41 +4,51 @@
  */
 ?>
 
-<section class="home-bannerz">
-	<div class="home-banner-text">
-		<?php			
-			$query = get_posts(['post_type'=>'home_page']);
-				foreach($query as $input): ?>
+<?php
 
-			<p><?php echo $input->banner_message ?></p>
-		
-		<?php endforeach; ?>
-	</div>
-	<div class="home-services-but">
-		<a href="/services"><p>SERVICES</p></a>
-	</div>
-</section>
+if( have_posts() ){
+	while(have_posts() ){
+		the_post();
+?>
 
-<section class="home-giant-menu">
-	<div class="home-big-about big-menu">
-		<div class="big-icon">
-			<a href="/about"><img src="<?php echo get_template_directory_uri() . '/dist/images/big_about.png'; ?>"></a>
-			<div class="big-icon-hover">
+		<section class="home-bannerz">
+			<div class="home-banner-text">
+				<p><?php echo get_field('banner_title', $post->ID); ?></p>
 			</div>
-		</div>
-	</div>
-	<div class="home-big-services big-menu">
-		<div class="big-icon">
-			<a href="/services"><img src="<?php echo get_template_directory_uri() . '/dist/images/big_services.png'; ?>"></a>
-			<div class="big-icon-hover">
+			<div class="home-services-but">
+				<a href="/services"><p>SERVICES</p></a>
 			</div>
-		</div>
-	</div>
-	<div class="home-big-contact big-menu">
-		<div class="big-icon">
-			<a href="/contact-us"><img src="<?php echo get_template_directory_uri() . '/dist/images/big_contact.png'; ?>"></a>
-			<div class="big-icon-hover">
+		</section>
+
+		<section class="home-giant-menu">
+			<div class="home-big-about big-menu">
+				<div class="big-icon">
+					<a href="/about"><img src="<?php echo get_template_directory_uri() . '/dist/images/big_about.png'; ?>"></a>
+					<div class="big-icon-hover">
+					</div>
+				</div>
 			</div>
-		</div>
-	</div>
-</section>
+			<div class="home-big-services big-menu">
+				<div class="big-icon">
+					<a href="/services"><img src="<?php echo get_template_directory_uri() . '/dist/images/big_services.png'; ?>"></a>
+					<div class="big-icon-hover">
+					</div>
+				</div>
+			</div>
+			<div class="home-big-contact big-menu">
+				<div class="big-icon">
+					<a href="/contact-us"><img src="<?php echo get_template_directory_uri() . '/dist/images/big_contact.png'; ?>"></a>
+					<div class="big-icon-hover">
+					</div>
+				</div>
+			</div>
+		</section>
+
+<?php
+		}
+	}
+
+?>
+
+
+
