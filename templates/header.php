b7fe04aeaac1b5f889043b3d7b0b24980254ce4c
Armando Bulnes <armandogbulnesjr@gmail.com>
2016-01-11 11:10:28 -0600

    made some mobile menu changes

--- templates/header.php
+++ templates/header.php
@@ -11,6 +11,13 @@
 		      	</div> 
 		      </a>
 	      </div>
+	      <div class="mobile-nav">
+	       <?php
+		      if (has_nav_menu('primary_navigation')) :
+		        wp_nav_menu(['theme_location' => 'primary_navigation','menu_class' => 'mobile-menu']);
+		      endif;
+		      ?>
+	      </div>
 	      <div class="hamburger-nav">
 	      	<i class="fa fa-bars"></i>
 	      </div>
